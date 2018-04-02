334725b4f14dcb5635c38e7b7aff858673653c32
Dero Kratzberg <bonsai.jem@gmail.com>
2018-04-02 17:05:44 -0500

    Add project files.

---
+++ ByTheChimney/ByTheChimney_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\bonsa\source\repos\ByTheChimney\ByTheChimney\ByTheChimney_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
