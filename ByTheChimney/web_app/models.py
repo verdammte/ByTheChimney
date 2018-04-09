288b741d0ec7248239e6dad0f629364785ca09fd
Dero Kratzberg <bonsai.jem@gmail.com>
2018-04-09 01:11:14 -0500

    added Event model, several self strings, getUpcoming(), and started preparing for index.html

--- ByTheChimney/web_app/models.py
+++ ByTheChimney/web_app/models.py
@@ -28,10 +28,16 @@ class Purchase(models.Model):
     DateOfPurchase = models.DateTimeField()
     Location = models.CharField(max_length=30)
     SpecialEvent = models.BooleanField()
-    CustomerName = models.CharField(max_length=50)
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
 
     def __str__(self):
         return (self.CustomerName + self.DateOfPurchase.strftime('%Y-%m-%d %H:%M'))
+
+    @property
+    def full_name(self):
+        #Returns the person's full name.
+        return '%s %s' % (self.first_name, self.last_name)
 
 class Transaction(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -39,5 +45,25 @@ class Transaction(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     refunded = models.BooleanField()
     dateOfRefund = models.DateTimeField()
+
+    def __str__(self):
+        return '%s, %s' (self.product, str(self.price))
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    recurring = models.BooleanField()
+    location = models.CharField(max_length=75)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    createDate = models.DateTimeField()
+    rainDate = models.DateTimeField()
+    isDelayed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    def getUpcoming(self):
+        return null
+
+
 
     
--- ByTheChimney/web_app/views.py
+++ ByTheChimney/web_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
+from .models import Product
 
 
 def index(request):
-    return render(request, 'polls/index.html')
+    events = get_object_or_404(Event.getUpcoming())
+
+    return render(request, 'polls/index.html', {'events': events})
 
 def store(request):
     return render(request, 'polls/store.html')
